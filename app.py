b174da994f50a630faa64c53c2d0b3dfd073456c
EzekielSealtien <EzekielTakam@teccart.online>
2025-04-02 02:38:04 -0400

    Update

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import streamlit as st
-from PIL import Image
 from ultralytics import YOLO
 import os
 import shutil
 from datetime import datetime
 
 # Load YOLOv8 model
-model = YOLO("best.pt")  # Make sure 'best.pt' is in the same folder as this script
+model = YOLO("best.pt") 
 
 st.set_page_config(page_title="Car Part Detector", layout="centered")
 st.title(" Car Part Detection with YOLOv8")
@@ -20,17 +19,15 @@ if uploaded_file is not None:
     with open(file_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
 
-    # Run inference
+    
     with st.spinner("Running detection..."):
         results = model(file_path, save=True, line_width=1)
         output_dir = results[0].save_dir
         output_file = os.path.join(output_dir, os.path.basename(file_path))
 
-    # Show results
-    st.success(" Detection complete!")
     st.image(output_file, caption="Detection Result", use_column_width=True)
 
-    # Cleanup (optional)
+    # Cleanup 
     if st.button("Clear temporary files"):
         shutil.rmtree("runs/detect")
         os.remove(file_path)
